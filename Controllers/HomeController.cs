3de34bff8e68c8d175d7e50bc189939d0debda0f
110189@st-maartenscollege.nl <110189@st-maartenscollege.nl>
2020-01-14 09:54:59 +0100

    Veel dingen veranderd

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,16 +11,21 @@ namespace SchoolTemplate.Controllers
   public class HomeController : Controller
   {
     // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
-    string connectionString = "Server=172.16.160.21;Port=3306;Database=;Uid=;Pwd=;";
+    string connectionString = "Server=172.16.160.21;Port=3306;Database=110189;Uid=110189;Pwd=antHEaNt;";
 
     public IActionResult Index()
     {
       List<Product> products = new List<Product>();
       // uncomment deze regel om producten uit je database toe te voegen
-      // products = GetProducts();
+      products = GetProducts();
 
       return View(products);
-    }
+    } 
+
+    public IActionResult ShowAll()
+        {
+            return View();
+        }
 
     private List<Product> GetProducts()
     {
@@ -40,9 +45,9 @@ namespace SchoolTemplate.Controllers
               Id = Convert.ToInt32(reader["Id"]),
               Naam = reader["Naam"].ToString(),
               Calorieen = float.Parse(reader["calorieen"].ToString()),
-              Formaat = reader["Naam"].ToString(),
-              Gewicht = Convert.ToInt32(reader["Naam"].ToString()),
-              Prijs = Decimal.Parse(reader["Naam"].ToString())
+              Formaat = reader["Formaat"].ToString(),
+              Gewicht = Convert.ToInt32(reader["Gewicht"].ToString()),
+              Prijs = Decimal.Parse(reader["Prijs"].ToString())
             };
             products.Add(p);
           }
