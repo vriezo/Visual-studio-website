1a7af03e3a879fe01439f59abaf1a7445bea14d3
110189@st-maartenscollege.nl <110189@st-maartenscollege.nl>
2020-09-11 14:56:25 +0200

    Contact pagina uitgebreid

--- Models/PersonModel.cs
+++ Models/PersonModel.cs
@@ -5,6 +5,7 @@ namespace SchoolTemplate.Models
 {
     public class PersonModel
     {
+        [Required(ErrorMessage = "Voornaam is verplicht")]
         public string Voornaam { get; set;  }
         
         [Required(ErrorMessage = "Achternaam is verplicht")]
@@ -14,6 +15,16 @@ namespace SchoolTemplate.Models
         [EmailAddress(ErrorMessage = "Geen geldig e-mail adres")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Geboortedatum is verplicht")]
         public DateTime Geboortedatum { get; set; }
+
+        [Required(ErrorMessage = "Adres is verplicht")]
+        public string Adres { get; set; }
+
+        [Required(ErrorMessage = "Woonplaats is verplicht")]
+        public string Woonplaats { get; set; }
+
+        [Required(ErrorMessage = "Postcode is verplicht")]
+        public string Postcode { get; set; }
     }
 }
